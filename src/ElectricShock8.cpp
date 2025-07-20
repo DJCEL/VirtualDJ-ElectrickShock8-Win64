c17090d6427621fcea03d205179cfce56cf42fa5
DJCEL <16272026+DJCEL@users.noreply.github.com>
2025-07-20 19:02:26 +0200

    Add files via upload

---
+++ src/ElectricShock8.cpp
@@ -0,0 +1,428 @@
+#include "ElectricShock8.h"
+
+
+//------------------------------------------------------------------------------------------
+CElectricShock8::CElectricShock8()
+{
+	pD3DDevice = nullptr; 
+	pD3DDeviceContext = nullptr;
+	pNewVertexBuffer = nullptr;
+	pPixelShader = nullptr;
+	pD3DRenderTargetView = nullptr;
+	pBlendState = nullptr;
+	ZeroMemory(pNewVertices, 6 * sizeof(TVertex8));
+	ZeroMemory(m_SliderValue, 3 * sizeof(float));
+	m_DirectX_On = false;
+	m_WidthOnDeviceInit = 0;
+	m_HeightOnDeviceInit = 0;
+	m_Width = 0;
+	m_Height = 0;
+	m_VertexCount = 0;
+	m_alpha = 1.0f;
+	m_length = 0.0f;
+	m_VideoScale = 1.0f;
+}
+//------------------------------------------------------------------------------------------
+CElectricShock8::~CElectricShock8()
+{
+
+}
+//------------------------------------------------------------------------------------------
+HRESULT VDJ_API CElectricShock8::OnLoad()
+{
+	HRESULT hr = S_FALSE;
+
+	hr = DeclareParameterSlider(&m_SliderValue[0], ID_SLIDER_1, "Alpha", "A", 0.0f);
+	hr = DeclareParameterSlider(&m_SliderValue[1], ID_SLIDER_2, "VideoScale", "VS", 0.3f);
+	hr = DeclareParameterSlider(&m_SliderValue[2], ID_SLIDER_3, "Length", "L", 0.25f);
+
+	OnParameter(ID_INIT);
+	return S_OK;
+}
+//------------------------------------------------------------------------------------------
+HRESULT VDJ_API CElectricShock8::OnGetPluginInfo(TVdjPluginInfo8 *info)
+{
+	info->Author = "djcel";
+	info->PluginName = "ElectricShock8";
+	info->Description = "It acts like a negative effect at each beat.";
+	info->Flags = 0x00; // VDJFLAG_VIDEO_OUTPUTRESOLUTION | VDJFLAG_VIDEO_OUTPUTASPECTRATIO;
+	info->Version = "1.0 (64-bit)";
+
+	return S_OK;
+}
+//------------------------------------------------------------------------------------------
+ULONG VDJ_API CElectricShock8::Release()
+{
+	delete this;
+	return 0;
+}
+//------------------------------------------------------------------------------------------
+HRESULT VDJ_API CElectricShock8::OnParameter(int id)
+{
+	if (id == ID_INIT)
+	{
+		for (int i = ID_SLIDER_1; i <= ID_SLIDER_3; i++) OnSlider(i);
+	}
+
+	OnSlider(id);
+
+	return S_OK;
+}
+//------------------------------------------------------------------------------------------
+void CElectricShock8::OnSlider(int id)
+{
+	switch (id)
+	{
+		case ID_SLIDER_1:
+			m_alpha = 1.0f - m_SliderValue[0];
+			break;
+
+		case ID_SLIDER_2:
+			m_VideoScale = m_SliderValue[1] * 0.5f;
+			break;
+
+		case ID_SLIDER_3:
+			m_length = m_SliderValue[2];
+			break;
+
+	}
+}
+//-------------------------------------------------------------------------------------------
+HRESULT VDJ_API CElectricShock8::OnGetParameterString(int id, char* outParam, int outParamSize)
+{
+	switch (id)
+	{
+		case ID_SLIDER_1:
+			sprintf_s(outParam, outParamSize, "%.0f%%", (1.0f - m_alpha) * 100);
+			break;
+
+		case ID_SLIDER_2:
+			sprintf_s(outParam, outParamSize, "%.2f", m_VideoScale);
+			break;
+
+		case ID_SLIDER_3:
+			sprintf_s(outParam, outParamSize, "%.2f beat", m_length);
+			break;
+
+	}
+
+	return S_OK;
+}
+//-------------------------------------------------------------------------------------------
+HRESULT VDJ_API CElectricShock8::OnDeviceInit()
+{
+	HRESULT hr = S_FALSE;
+
+	m_DirectX_On = true;
+	m_WidthOnDeviceInit = width;
+	m_HeightOnDeviceInit = height;
+	m_Width = width;
+	m_Height = height;
+
+	hr = GetDevice(VdjVideoEngineDirectX11, (void**)  &pD3DDevice);
+	if(hr!=S_OK || pD3DDevice==NULL) return E_FAIL;
+
+	hr = Initialize_D3D11(pD3DDevice);
+
+	return S_OK;
+}
+//-------------------------------------------------------------------------------------------
+HRESULT VDJ_API CElectricShock8::OnDeviceClose()
+{
+	Release_D3D11();
+	SAFE_RELEASE(pD3DRenderTargetView);
+	SAFE_RELEASE(pD3DDeviceContext);
+	m_DirectX_On = false;
+	
+	return S_OK;
+}
+//-------------------------------------------------------------------------------------------
+HRESULT VDJ_API CElectricShock8::OnStart() 
+{
+	return S_OK;
+}
+//-------------------------------------------------------------------------------------------
+HRESULT VDJ_API CElectricShock8::OnStop() 
+{
+	return S_OK;
+}
+//-------------------------------------------------------------------------------------------
+HRESULT VDJ_API CElectricShock8::OnDraw()
+{
+	HRESULT hr = S_FALSE;
+	ID3D11ShaderResourceView *pTexture = nullptr;
+	TVertex8* vertices = nullptr;
+
+	if (width != m_Width || height != m_Height)
+	{
+		OnResizeVideo();
+	}
+
+	hr = DrawDeck();
+	if (hr != S_OK) return S_FALSE;
+
+	// We get current texture and vertices
+	hr = GetTexture(VdjVideoEngineDirectX11, (void**) &pTexture, &vertices);
+	if (hr != S_OK) return S_FALSE;
+
+	pD3DDevice->GetImmediateContext(&pD3DDeviceContext);
+	if (!pD3DDeviceContext) return S_FALSE;
+
+	pD3DDeviceContext->OMGetRenderTargets(1, &pD3DRenderTargetView, nullptr);
+	if (!pD3DRenderTargetView) return S_FALSE;
+
+	hr = Rendering_D3D11(pD3DDevice, pD3DDeviceContext, pD3DRenderTargetView, pTexture, vertices);
+	if (hr != S_OK) return S_FALSE;
+
+	return S_OK;
+}
+//-----------------------------------------------------------------------
+HRESULT VDJ_API CElectricShock8::OnAudioSamples(float* buffer, int nb)
+{ 
+	//int FFT_SIZE = 512; // Size of the FFT (must be a power of 2)
+
+	//ComputeFFT(buffer, nb, FFT_SIZE);
+
+	return E_NOTIMPL;
+}
+//-----------------------------------------------------------------------
+void CElectricShock8::OnResizeVideo()
+{
+	m_Width = width;
+	m_Height = height;
+}
+//-----------------------------------------------------------------------
+HRESULT CElectricShock8::Initialize_D3D11(ID3D11Device* pDevice)
+{
+	HRESULT hr = S_FALSE;
+
+	hr = Create_VertexBufferDynamic_D3D11(pDevice);
+	if (hr != S_OK) return S_FALSE;
+
+	hr = Create_PixelShader_D3D11(pDevice);
+	if (hr != S_OK) return S_FALSE;
+
+	//hr = Create_BlendState_D3D11(pDevice);
+	//if (hr != S_OK) return S_FALSE;
+
+	return S_OK;
+}
+//-----------------------------------------------------------------------
+void CElectricShock8::Release_D3D11()
+{
+	SAFE_RELEASE(pNewVertexBuffer);
+	SAFE_RELEASE(pPixelShader);
+	//SAFE_RELEASE(pBlendState);
+}
+// -----------------------------------------------------------------------
+HRESULT CElectricShock8::Rendering_D3D11(ID3D11Device* pDevice, ID3D11DeviceContext* pDeviceContext, ID3D11RenderTargetView* pRenderTargetView, ID3D11ShaderResourceView* pTextureView, TVertex8* pVertices)
+{
+	HRESULT hr = S_FALSE;
+	D3DXCOLOR Color = {};
+
+
+	double fbp = SongPosBeats - floor(SongPosBeats);
+	if (fbp >= 0 && fbp < m_length)
+	{
+		Color = { 1.0f, 1.0f, 1.0f, m_alpha };
+	}
+	else
+	{
+		Color = { 0.0f, 0.0f, 0.0f, m_alpha };
+	}
+
+	if (pRenderTargetView)
+	{
+		FLOAT backgroundColor[4] = { Color.r, Color.g ,Color.b ,Color.a };
+		pDeviceContext->ClearRenderTargetView(pRenderTargetView, backgroundColor);
+
+		pDeviceContext->OMSetRenderTargets(1, &pRenderTargetView, nullptr);
+	}
+
+	hr = Update_VertexBufferDynamic_D3D11(pDeviceContext);
+	if (hr != S_OK) return S_FALSE;
+
+	
+	if (pPixelShader)
+	{
+		pDeviceContext->PSSetShader(pPixelShader, nullptr, 0);
+	}
+	
+	if (pTextureView)
+	{
+		pDeviceContext->PSSetShaderResources(0, 1, &pTextureView);
+	}
+
+	/*
+	if (pBlendState)
+	{
+		pDeviceContext->OMSetBlendState(pBlendState, nullptr, 0xFFFFFFFF);
+	}
+	*/
+	
+	if (pNewVertexBuffer)
+	{
+		UINT m_VertexStride = sizeof(TLVERTEX);
+		UINT m_VertexOffset = 0;
+		pDeviceContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+		pDeviceContext->IASetVertexBuffers(0, 1, &pNewVertexBuffer, &m_VertexStride, &m_VertexOffset);
+	}
+	
+	pDeviceContext->Draw(m_VertexCount, 0);
+	
+	return S_OK;
+}
+// ---------------------------------------------------------------------- -
+HRESULT CElectricShock8::Create_VertexBufferDynamic_D3D11(ID3D11Device* pDevice)
+{
+	HRESULT hr = S_FALSE;
+
+	if (!pDevice) return S_FALSE;
+
+	// Set the number of vertices in the vertex array.
+	m_VertexCount = 6; // = ARRAYSIZE(pNewVertices);
+	
+	// Fill in a buffer description.
+	D3D11_BUFFER_DESC VertexBufferDesc;
+	ZeroMemory(&VertexBufferDesc, sizeof(VertexBufferDesc));
+	VertexBufferDesc.Usage = D3D11_USAGE_DYNAMIC;   // CPU_Access=Write_Only & GPU_Access=Read_Only
+	VertexBufferDesc.ByteWidth = sizeof(TLVERTEX) * m_VertexCount;
+	VertexBufferDesc.BindFlags = D3D11_BIND_VERTEX_BUFFER; //D3D11_BIND_INDEX_BUFFER
+	VertexBufferDesc.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE; // Allow CPU to write in buffer
+	VertexBufferDesc.MiscFlags = 0;
+
+	hr = pDevice->CreateBuffer(&VertexBufferDesc, NULL, &pNewVertexBuffer);
+	if (hr != S_OK || !pNewVertexBuffer) return S_FALSE;
+
+	return S_OK;
+}
+//-----------------------------------------------------------------------
+HRESULT CElectricShock8::Update_VertexBufferDynamic_D3D11(ID3D11DeviceContext* ctx)
+{
+	HRESULT hr = S_FALSE;
+
+	if (!ctx) return S_FALSE;
+	if (!pNewVertexBuffer) return S_FALSE;
+
+	D3D11_MAPPED_SUBRESOURCE MappedSubResource;
+	ZeroMemory(&MappedSubResource, sizeof(D3D11_MAPPED_SUBRESOURCE));
+
+
+	hr = ctx->Map(pNewVertexBuffer, NULL, D3D11_MAP_WRITE_DISCARD, 0, &MappedSubResource);
+	if (hr != S_OK) return S_FALSE;
+
+	//TLVERTEX* pNewVertices = (TLVERTEX*) MappedSubResource.pData;
+
+	hr = Update_Vertices_D3D11();
+
+	memcpy(MappedSubResource.pData, pNewVertices, m_VertexCount * sizeof(TLVERTEX));
+
+	ctx->Unmap(pNewVertexBuffer, NULL);
+
+	return S_OK;
+}
+//-----------------------------------------------------------------------
+HRESULT CElectricShock8::Update_Vertices_D3D11()
+{
+	float frameWidth = (float) m_Width;
+	float frameHeight = (float) m_Height;
+
+	float deltaWidth = frameWidth * m_VideoScale;
+	float deltaHeight = frameHeight * m_VideoScale;
+
+	D3DXPOSITION P1 = { 0.0f + deltaWidth, 0.0f + deltaHeight, 0.0f }, // Top Left
+		P2 = { 0.0f + deltaWidth, frameHeight - deltaHeight, 0.0f }, // Bottom Left
+		P3 = { frameWidth - deltaWidth, 0.0f + deltaHeight, 0.0f }, // Top Right
+		P4 = { frameWidth - deltaWidth, frameHeight - deltaHeight, 0.0f }; // Bottom Right
+	D3DXCOLOR color_vertex = D3DXCOLOR(1.0f, 1.0f, 1.0f, m_alpha); // White color with alpha layer
+	D3DXTEXCOORD T1 = { 0.0f , 0.0f }, T2 = { 0.0f , 1.0f }, T3 = { 1.0f , 0.0f }, T4 = { 1.0f , 1.0f };
+
+	// Triangle n°1 (Bottom Right)
+	pNewVertices[0] = { P3 , color_vertex , T3 };
+	pNewVertices[1] = { P4 , color_vertex , T4 };
+	pNewVertices[2] = { P2 , color_vertex , T2 };
+
+	// Triangle n°2 (Top Left)
+	pNewVertices[3] = { P2 , color_vertex , T2 };
+	pNewVertices[4] = { P1 , color_vertex , T1 };
+	pNewVertices[5] = { P3 , color_vertex , T3 };
+
+
+	return S_OK;
+}
+//-----------------------------------------------------------------------
+HRESULT CElectricShock8::Create_PixelShader_D3D11(ID3D11Device* pDevice)
+{
+	HRESULT hr = S_FALSE;
+	const WCHAR* pShaderHLSLFilepath = L"PixelShader.hlsl";
+	const WCHAR* pShaderCSOFilepath = L"PixelShader.cso";
+	const WCHAR* resourceType = RT_RCDATA;
+	const WCHAR* resourceName = L"PIXELSHADER_CSO";
+
+	SAFE_RELEASE(pPixelShader);
+
+	hr = Create_PixelShaderFromResourceCSOFile_D3D11(pDevice, resourceType, resourceName);
+
+	return hr;
+}
+//-----------------------------------------------------------------------
+HRESULT CElectricShock8::Create_PixelShaderFromResourceCSOFile_D3D11(ID3D11Device* pDevice, const WCHAR* resourceType, const WCHAR* resourceName)
+{
+	HRESULT hr = S_FALSE;
+	
+	void* pShaderBytecode = nullptr;
+	SIZE_T BytecodeLength = 0;
+
+	hr = ReadResource(resourceType, resourceName, &BytecodeLength, &pShaderBytecode);
+	if (hr != S_OK) return S_FALSE;
+		
+	hr = pDevice->CreatePixelShader(pShaderBytecode, BytecodeLength, nullptr, &pPixelShader);
+
+	return hr;
+}
+//-----------------------------------------------------------------------
+HRESULT CElectricShock8::ReadResource(const WCHAR* resourceType, const WCHAR* resourceName, SIZE_T* size, LPVOID *data)
+{
+	HRESULT hr = S_FALSE;
+
+	HRSRC rc = FindResource(hInstance, resourceName, resourceType);
+	if (!rc) return S_FALSE;
+
+	HGLOBAL rcData = LoadResource(hInstance, rc);
+	if (!rcData) return S_FALSE;
+
+	*size = (SIZE_T)SizeofResource(hInstance, rc);
+	if (*size == 0) return S_FALSE;
+
+	*data = LockResource(rcData);
+	if (*data == nullptr) return S_FALSE;
+
+	return S_OK;
+}
+//-----------------------------------------------------------------------
+HRESULT CElectricShock8::Create_BlendState_D3D11(ID3D11Device* pDevice)
+{
+	HRESULT hr = S_FALSE;
+
+	D3D11_RENDER_TARGET_BLEND_DESC RenderTargetBlendDesc;
+	ZeroMemory(&RenderTargetBlendDesc, sizeof(D3D11_RENDER_TARGET_BLEND_DESC));
+	RenderTargetBlendDesc.BlendEnable = TRUE;
+	RenderTargetBlendDesc.SrcBlend = D3D11_BLEND_SRC_COLOR; // The data source is color data (RGB) from a pixel shader. No pre-blend operation.
+	RenderTargetBlendDesc.DestBlend = D3D11_BLEND_DEST_COLOR; // The data source is color data from a rendertarget. No pre-blend operation.
+	RenderTargetBlendDesc.BlendOp = D3D11_BLEND_OP_ADD;
+	RenderTargetBlendDesc.SrcBlendAlpha = D3D11_BLEND_SRC_ALPHA; // The data source is alpha data from a pixel shader. No pre-blend operation.
+	RenderTargetBlendDesc.DestBlendAlpha = D3D11_BLEND_DEST_ALPHA; // The data source is alpha data from a rendertarget. No pre-blend operation. 
+	RenderTargetBlendDesc.BlendOpAlpha = D3D11_BLEND_OP_ADD;
+	RenderTargetBlendDesc.RenderTargetWriteMask = D3D11_COLOR_WRITE_ENABLE_ALPHA; // D3D11_COLOR_WRITE_ENABLE_ALL
+
+
+	D3D11_BLEND_DESC BlendStateDesc;
+	ZeroMemory(&BlendStateDesc, sizeof(D3D11_BLEND_DESC));
+	BlendStateDesc.AlphaToCoverageEnable = FALSE;
+	BlendStateDesc.IndependentBlendEnable = FALSE;
+	BlendStateDesc.RenderTarget[0] = RenderTargetBlendDesc;
+
+	hr = pDevice->CreateBlendState(&BlendStateDesc, &pBlendState);
+
+	return hr;
+}
---
+++ src/ElectricShock8.h
@@ -0,0 +1,121 @@
+#ifndef ELECTRICSHOCK8_H
+#define ELECTRICSHOCK8_H
+
+
+#include "vdjVideo8.h"
+#include <d3d11.h>
+#include <math.h> // for the function floor()
+#include <stdio.h>
+
+#pragma comment(lib, "d3d11.lib")
+
+
+//////////////////////////////////////////////////////////////////////////
+// Class definition
+//////////////////////////////////////////////////////////////////////////
+class CElectricShock8 : public IVdjPluginVideoFx8
+{
+public:
+	CElectricShock8();
+	~CElectricShock8();
+	HRESULT VDJ_API OnLoad();
+	HRESULT VDJ_API OnGetPluginInfo(TVdjPluginInfo8 *info);
+	ULONG   VDJ_API Release();
+	HRESULT VDJ_API OnParameter(int id);
+	HRESULT VDJ_API OnGetParameterString(int id, char* outParam, int outParamSize);
+	HRESULT VDJ_API OnDeviceInit();
+	HRESULT VDJ_API OnDeviceClose();
+	HRESULT VDJ_API OnDraw();
+	HRESULT VDJ_API OnStart();
+	HRESULT VDJ_API OnStop();
+	HRESULT VDJ_API OnAudioSamples(float* buffer, int nb);
+
+private:
+	struct D3DXPOSITION
+	{
+		float x;
+		float y;
+		float z;
+	};
+	struct D3DXCOLOR
+	{
+	public:
+		D3DXCOLOR() = default;
+		D3DXCOLOR(FLOAT r, FLOAT g, FLOAT b, FLOAT a)
+		{
+			this->r = r;
+			this->g = g;
+			this->b = b;
+			this->a = a;
+		}
+
+		operator FLOAT* ()
+		{
+			return &r;
+		}
+
+		FLOAT r, g, b, a;
+	};
+	struct D3DXTEXCOORD
+	{
+		float tu;
+		float tv;
+	};
+
+	struct TLVERTEX
+	{
+		D3DXPOSITION position;
+		D3DXCOLOR color;
+		D3DXTEXCOORD texture;
+	};
+
+	
+	void OnResizeVideo();
+	void OnSlider(int id);
+	HRESULT ReadResource(const WCHAR* resourceType, const WCHAR* resourceName, SIZE_T* size, LPVOID* data);
+
+	HRESULT Initialize_D3D11(ID3D11Device* pDevice);
+	void Release_D3D11();
+	HRESULT Rendering_D3D11(ID3D11Device* pDevice, ID3D11DeviceContext* pDeviceContext, ID3D11RenderTargetView* pRenderTargetView, ID3D11ShaderResourceView* pTextureView, TVertex8* pVertices);
+	HRESULT Create_PixelShader_D3D11(ID3D11Device* pDevice);
+	HRESULT Create_PixelShaderFromResourceCSOFile_D3D11(ID3D11Device* pDevice, const WCHAR* resourceType, const WCHAR* resourceName);
+	HRESULT Create_VertexBufferDynamic_D3D11(ID3D11Device* pDevice);
+	HRESULT Update_VertexBufferDynamic_D3D11(ID3D11DeviceContext* ctx);
+	HRESULT Update_Vertices_D3D11();
+	HRESULT Create_BlendState_D3D11(ID3D11Device* pDevice);
+
+	
+	ID3D11Device* pD3DDevice;
+	ID3D11DeviceContext* pD3DDeviceContext;
+	ID3D11RenderTargetView* pD3DRenderTargetView;
+	ID3D11Buffer* pNewVertexBuffer;
+	ID3D11PixelShader* pPixelShader;
+	ID3D11BlendState* pBlendState;
+	
+	TLVERTEX pNewVertices[6];
+	UINT m_VertexCount;
+	bool m_DirectX_On;
+	int m_WidthOnDeviceInit;
+	int m_HeightOnDeviceInit;
+	int m_Width;
+	int m_Height;
+	float m_SliderValue[3];
+	float m_alpha;
+	float m_length;
+	float m_VideoScale;
+
+	typedef enum _ID_Interface
+	{
+		ID_INIT,
+		ID_SLIDER_1,
+		ID_SLIDER_2,
+		ID_SLIDER_3
+	} ID_Interface;
+
+	#ifndef SAFE_RELEASE
+	#define SAFE_RELEASE(x) { if (x!=nullptr) { x->Release(); x=nullptr; } }
+	#endif
+
+};
+
+#endif
